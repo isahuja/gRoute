3204c18b394b8d0bfa306bb33375a7f484129b52
Inder <isahuja1@yahoo.com>
2017-08-20 16:43:08 +0530

    initial setup

---
+++ resources/views/search.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if IE 9]>         <html class="ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+
+<html class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta name="msvalidate.01" content="A848D1242C679C3E83AFBF7842016E6C" />
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" /><meta name="keywords" content="Alternate Route"/>
+	<meta http-equiv="content-language" content="en" />
+	<link rel="stylesheet" href="css/app.css?v=1">
+	<script src="js/app.js?v=1"></script>
+	<title>Search</title>
+</head>
+<body>
+
+	<div class="container">
+		<div class="centered">
+			<form action="">
+				<div class="form-group col-xs-5">
+				  <label for="example-text-input" class="col-2 col-form-label">Starting Point</label>
+				  <div class="col-10">
+				    <input class="form-control" type="text" value="" id="example-text-input" placeholder="starting point">
+				  </div>
+				</div>
+
+				<div class="form-group col-xs-5">
+				  <label for="example-text-input" class="col-2 col-form-label">Destination Point</label>
+				  <div class="col-10">
+				    <input class="form-control" type="text" value="" id="example-text-input" placeholder="destination point">
+				  </div>
+				</div>
+
+				<div class="form-group col-xs-2">
+					<button type="submit" class="btn btn-primary submitsearch">Submit</button>
+				</div>
+
+			</form>
+		</div>
+	</div>
+</body>
+</html>
